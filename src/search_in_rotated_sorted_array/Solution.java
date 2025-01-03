593bc11c836aa63952b438e62f74300f2ad4c45e
lukasnt <lukastveiten@hotmail.no>
2025-01-03 15:01:00 +0100

    Add search_in_rotated_sorted_array/Solution

--- src/search_in_rotated_sorted_array/Solution.java
+++ src/search_in_rotated_sorted_array/Solution.java
@@ -9,7 +9,7 @@ class Solution {
         if (nums.length == 2) {
             return (nums[0] == target) ? 0 : (nums[1] == target ? 1 : -1);
         }
-        System.out.println(Arrays.toString(nums));
+        //System.out.printf("nums: %s, target: %s\n", Arrays.toString(nums), target);
         int kLow = 0;
         int kHigh = nums.length - 1;
         int k = kLow + (kHigh - kLow) / 2;
@@ -19,29 +19,34 @@ class Solution {
             } else {
                 kHigh = k;
             }
-            System.out.printf("kLow: %d, kHigh: %d, k: %d\n", kLow, kHigh, k);
+            //System.out.printf("kLow: %d, kHigh: %d, k: %d\n", kLow, kHigh, k);
             k = kLow + (kHigh - kLow) / 2;
         }
         if (nums.length > 1 && nums[0] < nums[nums.length - 1]) k = 0;
         else k++;
-        System.out.println(k);
 
         int low = 0;
         int high = nums.length - 1;
         int mid = low + (high - low) / 2;
         while (high - low > 1) {
-            if (nums[(mid + k) % nums.length] == target) {
-                break;
-            } else if (nums[(mid + k) % nums.length] > target) {
+            if (nums[(mid + k) % nums.length] >= target) {
                 high = mid;
             } else {
-                low = mid;
+               low = mid;
             }
-            System.out.printf("low: %d, high: %d, mid: %d\n", low, high, mid);
+            //System.out.printf("low: %d, high: %d, mid: %d\n", low, high, mid);
             mid = low + (high - low) / 2;
+
+            if (nums[(low + k) % nums.length] == target) {
+                mid = low;
+                break;
+            } else if (nums[(high + k) % nums.length] == target) {
+                mid = high;
+                break;
+            }
         }
         int result = nums[(mid + k) % nums.length] == target ? (mid + k) % nums.length : -1;
-        System.out.printf("result: %d\n", result);
+        //System.out.printf("result: %d\n", result);
         return result;
     }
 
@@ -60,6 +65,9 @@ class Solution {
         search(new int[] {1,3}, 1);
         search(new int[] {4,5,6,7,0,1,2}, 3);
         search(new int[] {1,3,5}, 5);
+        search(new int[] {1,3,5}, 3);
+        search(new int[] {3}, 3);
+        search(new int[] {3,5}, 3);
 
     }
 }
